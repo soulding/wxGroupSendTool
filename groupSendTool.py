d83005325a6df6433ba6418be8143f91b21b3f7c
dzy <dzy0112@163.com>
2019-01-16 08:48:22 +0800

    添加控制发送群组和好友代码

--- groupSendTool.py
+++ groupSendTool.py
@@ -9,18 +9,30 @@ print('*****此软件仅个人使用，请勿传播!---By Soulding*****')
 print('***************************************************')
 print('\n-------------------编辑发送内容--------------------------------')
 
-while 1:
+while True:
 	msg = input('\n输入要发送的信息(空格表示换行，多个空格多次换行)：\n')
 	msg1 = msg.replace(' ','\n')
+	flag = input('\n选择发送类型：1 全部好友 2 全部群组 3 全部好友+群组：\n')
 	confirm = input('\n确认发送吗？:(y/n)\n')
 	if confirm == 'y' or confirm == 'Y':
 		break
-#发送给全部好友
-for friend in friends:
-	friend.send(msg1)
-#发送给所有群组
-for group in groups:
-	group.send(msg1)
+flag = int(flag)
+if(flag == 1):
+	print('全部好友\n')
+	for friend in friends:		
+	 	friend.send(msg1)
+if(flag == 2):
+	print('全部群组\n')
+	for group in groups:		
+	 	group.send(msg1)
+if(flag == 3):
+	print('全部好友+群组\n')
+	#发送给全部好友
+	for friend in friends:		
+	 	friend.send(msg1)
+	#发送给所有群组
+	for group in groups:
+	 	group.send(msg1)
 print('\n-------------------发送的内容----------------------------------')
 print(msg1)
 print('\n-------------------发送的结果----------------------------------')
